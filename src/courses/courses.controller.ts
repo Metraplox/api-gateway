2aa0bc9c7725c60c679f785678f12fcaac788b7d
cristiannZzzz <cristian.bulla02@gmail.com>
2024-12-09 18:30:34 -0300

    fix: :bug: funcionando crud de cursos

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,45 +1,48 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { firstValueFrom, Observable } from 'rxjs';
 import { CoursesMSG } from 'src/common/constants';
 import { ClientProxyApp } from 'src/common/proxy/client-proxy';
 import { CreateCourseDto, UpdateCourseDto } from './dto/course.dto';
+import { IUser } from 'src/common/interfaces/user.interface';
 
 @Controller('courses')
 export class CoursesController {
   constructor(private readonly clientProxy: ClientProxyApp) {}
-  private _clientProxyUser = this.clientProxy.clientProxyUsers();
+  private _clientProxyCourses = this.clientProxy.clientProxyCourses();
 
   @Post('create')
   async createCourse(@Body() createCourseInput: CreateCourseDto): Promise<any> {
     return await firstValueFrom(
-      this._clientProxyUser.send(CoursesMSG.CREATE, { ...createCourseInput }),
-    );
-  }
-
-  @Post('find/:id')
-  async findCourse(@Param('id') id: string): Promise<any> {
-    return await firstValueFrom(
-      this._clientProxyUser.send(CoursesMSG.FIND_ONE, { id }),
-    );
-  }
-
-  @Post('update/:id')
-  async updateCourse(
-    @Param('id') id: string,
-    @Body() updateCourseInput: UpdateCourseDto,
-  ): Promise<any> {
-    return await firstValueFrom(
-      this._clientProxyUser.send(CoursesMSG.UPDATE, {
-        id,
-        ...updateCourseInput,
+      this._clientProxyCourses.send(CoursesMSG.CREATE, {
+        ...createCourseInput,
       }),
     );
   }
 
+  @Get()
+  findAll(): Observable<IUser[]> {
+    console.log('find all');
+    return this._clientProxyCourses.send(CoursesMSG.FIND_ALL, {});
+  }
+
+  @Get('find/:id')
+  findCourse(@Param('id') id: string): Observable<any> {
+    return this._clientProxyCourses.send(CoursesMSG.FIND_ONE, { id });
+  }
+
+  @Post('update/:id')
+  updateCourse(
+    @Param('id') id: string,
+    @Body() updateCourseInput: UpdateCourseDto,
+  ): Observable<any> {
+    return this._clientProxyCourses.send(CoursesMSG.UPDATE, {
+      id,
+      ...updateCourseInput,
+    });
+  }
+
   @Post('delete/:id')
-  async deleteCourse(@Param() id: string): Promise<any> {
-    return await firstValueFrom(
-      this._clientProxyUser.send(CoursesMSG.DELETE, { id }),
-    );
+  deleteCourse(@Param() id: string): Observable<any> {
+    return this._clientProxyCourses.send(CoursesMSG.DELETE, { id });
   }
 }
